bb6cda744babaf37bb198df097619c9624e2ead8
RickyCode <rdelriguzman@gmail.com>
2025-03-05 12:33:50 -0300

    chore(.standards): add the update_gitignore to repo preparation

--- prepare.py
+++ prepare.py
@@ -1,12 +1,14 @@
 from generate_context import main as generate_context
 from run_precommit import run_precommit
 from sync import sync_files, update_submodule
+from update_gitignore import update_gitignore
 
 
 def prepare_repo():
     update_submodule()
     sync_files()
     run_precommit()
+    update_gitignore()
     generate_context()
 
 
--- update_gitignore.py
+++ update_gitignore.py
@@ -1,10 +1,12 @@
 import os
+from utils import run_cmd
 
 gitignore_path = '.gitignore'
 file_to_ignore = 'project_context.md'
 
 
 def update_gitignore():
+    print('Updating .gitignore...')
     if not os.path.exists(gitignore_path):
         with open(gitignore_path, 'w') as gitignore_file:
             gitignore_file.write('')
@@ -15,6 +17,9 @@ def update_gitignore():
     if file_to_ignore not in [line.strip() for line in lines]:
         with open(gitignore_path, 'a') as gitignore_file:
             gitignore_file.write(f'\n{file_to_ignore}\n')
+
+    run_cmd(f'git add {gitignore_path}')
+    run_cmd(f'git commit -m "CHORE: Add {file_to_ignore} to .gitignore"')
 
 
 if __name__ == '__main__':
